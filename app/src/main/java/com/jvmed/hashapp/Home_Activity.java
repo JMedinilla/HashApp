15fa74a830f9ce01fde640a3119070909ff52a8c
JMedinilla <javimedinilla@gmail.com>
2016-10-15 15:51:28 +0200

    Some comments and Spanish language added. Now uses SnackBars instead of Toasts

--- app/src/main/java/com/jvmed/hashapp/Home_Activity.java
+++ app/src/main/java/com/jvmed/hashapp/Home_Activity.java
@@ -2,14 +2,15 @@ package com.jvmed.hashapp;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -17,6 +18,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Home_Activity extends AppCompatActivity {
 
+    private RelativeLayout mainLayout;
     private EditText edtOriginal;
     private TextView edtMD5;
     private TextView edtSHA1;
@@ -31,11 +33,16 @@ public class Home_Activity extends AppCompatActivity {
         initialize();
     }
 
-    private void sendMessage(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    /**
+     * Method that receive a string to show it as a SnackBar
+     * @param message Message to show
+     */
+    private void sendSnack(String message) {
+        Snackbar.make(mainLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 
     private void initialize() {
+        mainLayout = (RelativeLayout)findViewById(R.id.main_layout);
         edtOriginal = (EditText)findViewById(R.id.edtOriginal);
         edtMD5 = (TextView)findViewById(R.id.edtMD5);
         edtSHA1 = (TextView)findViewById(R.id.edtSHA1);
@@ -68,11 +75,16 @@ public class Home_Activity extends AppCompatActivity {
         });
     }
 
-    private String convertSHA256(String cadena) {
-        return bin2hex(getHash(cadena)).toLowerCase();
+    /**
+     * Method that receive the string to convert
+     * @param string Message to convert
+     * @return Calls the method that makes the conversion and returns it
+     */
+    private String convertSHA256(String string) {
+        return bin2hex(getHash(string)).toLowerCase();
     }
 
-    private byte[] getHash(String cadena) {
+    private byte[] getHash(String string) {
         MessageDigest digest=null;
         try {
             digest = MessageDigest.getInstance("SHA-256");
@@ -82,7 +94,7 @@ public class Home_Activity extends AppCompatActivity {
             digest.reset();
 
         if (digest != null) {
-            return digest.digest(cadena.getBytes());
+            return digest.digest(string.getBytes());
         }
         else
             return null;
@@ -92,6 +104,11 @@ public class Home_Activity extends AppCompatActivity {
         return String.format("%0" + (data.length * 2) + "X", new BigInteger(1, data));
     }
 
+    /**
+     * Method that receive the string to convert
+     * @param string Message to convert
+     * @return Calls the method that makes the conversion and returns it
+     */
     private String convertSHA1(String string) {
         MessageDigest digest = null;
         try {
@@ -120,6 +137,11 @@ public class Home_Activity extends AppCompatActivity {
         return buf.toString();
     }
 
+    /**
+     * Method that receive the string to convert
+     * @param string Message to convert
+     * @return New converted string in MD5
+     */
     private String convertMD5(String string) {
         final String MD5 = "MD5";
         try {
@@ -141,31 +163,36 @@ public class Home_Activity extends AppCompatActivity {
         return "";
     }
 
+    /**
+     * Method that receives the clipboard button's clicks
+     * They all copy plain text to the clipboard and send a SnackBar
+     * @param view Selected button
+     */
     public void getButtonClick (View view) {
         ClipData clip;
         switch (view.getId()) {
             case R.id.btnOriginal:
                 clip = ClipData.newPlainText("jv", edtOriginal.getText().toString());
                 clipboard.setPrimaryClip(clip);
-                sendMessage(getResources().getString(R.string.original_copied));
+                sendSnack(getResources().getString(R.string.original_copied));
                 break;
 
             case R.id.btnMD5:
                 clip = ClipData.newPlainText("jv", edtMD5.getText().toString());
                 clipboard.setPrimaryClip(clip);
-                sendMessage(getResources().getString(R.string.md5_copied));
+                sendSnack(getResources().getString(R.string.md5_copied));
                 break;
 
             case R.id.btnSHA1:
                 clip = ClipData.newPlainText("jv", edtSHA1.getText().toString());
                 clipboard.setPrimaryClip(clip);
-                sendMessage(getResources().getString(R.string.sha1_copied));
+                sendSnack(getResources().getString(R.string.sha1_copied));
                 break;
 
             case R.id.btnSHA256:
                 clip = ClipData.newPlainText("jv", edtSHA356.getText().toString());
                 clipboard.setPrimaryClip(clip);
-                sendMessage(getResources().getString(R.string.sha256_copied));
+                sendSnack(getResources().getString(R.string.sha256_copied));
                 break;
         }
     }
